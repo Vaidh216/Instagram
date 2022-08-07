bde51b7ff8b1a9a4e56936edcdedf5e5fef2b7a6
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-07 20:59:14 +0530

    3

--- core/views.py
+++ core/views.py
@@ -9,7 +9,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 def signup(request):
 
@@ -99,3 +101,7 @@ def settings(request):
         return redirect('settings')
 
     return render(request,'setting.html', {'user_profile':user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
