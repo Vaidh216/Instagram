4936cfd037c82b176e0a525a58e3ee73de7dc1dd
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-07 19:23:35 +0530

    2

--- core/models.py
+++ core/models.py
@@ -19,9 +19,12 @@ class Profile(models.Model):
         return self.user.username
 
 class Post(models.Model):
-    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
     image = models.ImageField(upload_to="post_images")
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
-    no_of_likes = 
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
