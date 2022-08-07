b2e52a4e24bf43e10c786defb00072c76f0ca325
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-07 18:04:38 +0530

    1

--- core/models.py
+++ core/models.py
@@ -1,5 +1,9 @@
+from asyncio.proactor_events import _ProactorBasePipeTransport
+from email.policy import default
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 User = get_user_model()
 
@@ -13,3 +17,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="post_images")
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = 
