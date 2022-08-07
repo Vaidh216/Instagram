dc475c978b52da0dc447e48bf14da5393e920a41
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-07 22:24:44 +0530

    1

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -104,4 +104,17 @@ def settings(request):
 
 @login_required(login_url='signin')
 def upload(request):
-    return HttpResponse('<h1>Upload View</h1>')
+
+    if request.method == "POST":
+        user =   request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+
+        return redirect("/")
+
+    else:
+        return redirect('/')
+    # return HttpResponse('<h1>Upload View</h1>')
