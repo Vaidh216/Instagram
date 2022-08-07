62d1ce7172db64ea21a9382fbd5227a4f27701ce
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-08 00:36:32 +0530

    2

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile, Post
+from .models import LikePost, Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -124,4 +124,25 @@ def upload(request):
 
 @login_required(login_url='signin')
 def like_post(request):
-    pass
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id,username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
+
+def profile(request, pk): 
+    return render(request, 'profile.html')
