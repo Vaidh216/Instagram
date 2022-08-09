d6a59671d7ed95fd20cb82ffb5903706e4f42029
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-09 14:58:16 +0530

    2

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 from django.views.static import serve
-from django.conf.urls import re_path
+from django.urls import re_path
 from django.conf import settings
 
 urlpatterns = [
