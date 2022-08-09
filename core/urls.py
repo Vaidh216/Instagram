3de8034b9a63bddaf7183e8050c5857046a6ef71
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-09 14:56:18 +0530

    3

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.conf import settings
 
 urlpatterns = [
@@ -17,6 +17,6 @@ urlpatterns = [
     path('follow',views.follow, name='follow'),
     path('search',views.search, name='search'),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
