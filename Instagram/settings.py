7f484587293bdbd541d69948b107ac8bd7d73526
Vaidh Prakash Singh <vedprakashsingh216@gmail.com>
2022-08-09 14:43:51 +0530

    5

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*b_p!f7q49)#ct!+nvx3a$c81#sx0b+k74&8#@!1!_q3=o0-3='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','testing-social.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('',views.index,name='index'),
@@ -12,4 +14,7 @@ urlpatterns = [
     path('profile/<str:pk>',views.profile, name='like-profile'),
     path('follow',views.follow, name='follow'),
     path('search',views.search, name='search'),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
